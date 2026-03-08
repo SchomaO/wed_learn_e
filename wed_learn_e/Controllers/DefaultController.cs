8018a4ea97442a18a8fd71285089c5762b05395f
TRUNG HIEU VO HO <soqua2k6@gmail.com>
2026-03-08 21:45:29 +0700

    update project
    Add DefaultController and update project structure
    
    Added DefaultController with Index and About actions. Registered the controller in the project file and created a Views\Default folder. Updated the solution file to assign a unique project Id. No changes to class.cs.

---
+++ wed_learn_e/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wed_learn_e.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+    }
+}
