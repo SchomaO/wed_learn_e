e55865414f94505004a5ebb01c78bd4485c8916e
TRUNG HIEU VO HO <soqua2k6@gmail.com>
2026-03-09 19:39:22 +0700

    Add Tranh_chuController and custom homepage view
    Added Tranh_chuController with Index action and created a new Index.cshtml view for the homepage ("Trang chủ") featuring a styled header and hero section. Updated the project file to include these new files.

---
+++ wed_learn_e/Controllers/Tranh_chuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wed_learn_e.Controllers
+{
+    public class Tranh_chuController : Controller
+    {
+        // GET: Tranh_chu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
