fd94b726249cec6c1fffb43b0f1e8d7054c44fd1
TRUNG HIEU VO HO <soqua2k6@gmail.com>
2026-03-09 21:49:35 +0700

    Add bai_test controller and view; update project files
    Added bai_testController with Index action and corresponding Index.cshtml view. Updated wed_learn_e.csproj to include new controller and view files. Also added Views\Trang_chu\Index.cshtml to project content.

---
+++ wed_learn_e/Controllers/bai_testController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wed_learn_e.Controllers
+{
+    public class bai_testController : Controller
+    {
+        // GET: bai_test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
