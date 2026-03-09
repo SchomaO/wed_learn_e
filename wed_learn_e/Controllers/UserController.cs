0fae3e9389de1b54c322fa193b72bc18e5222fbb
TRUNG HIEU VO HO <soqua2k6@gmail.com>
2026-03-09 20:39:51 +0700

    Add user registration/login views and update home layout
    Removed default Home views and references. Added UserController with registration and login actions, along with new user views and a shared layout for authentication pages. Updated main Index.cshtml with new footer and action buttons. Updated project file to reflect these changes.

---
+++ wed_learn_e/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wed_learn_e.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult dangky()
+        {
+
+            return View();
+        }
+        public ActionResult dangnhap()
+        {
+            return View();
+        }
+    }
+}
