8030f57f6b439cfb7b8d2ab564a5f68bea1a737d
TRUNG HIEU VO HO <soqua2k6@gmail.com>
2026-03-10 22:02:16 +0700

    Add quiz feature and migrate to Entity Framework 5
    Implemented "Bài Test" (quiz) functionality with new controller, views, and styles. Migrated Entity Framework to version 5, updated code generation templates, and adjusted model/context for EF5 compatibility. Refactored layouts, improved navigation, and updated project dependencies and configuration. Removed global _ViewStart.cshtml to support per-view layouts.

---
+++ wed_learn_e/Controllers/bai_testController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wed_learn_e.Controllers
+{
+    public class bai_testController : Controller
+    {
+        // GET: bai_test
+        public ActionResult nav_baitest()
+        {
+            return View();
+        }
+        public ActionResult bai_test()
+        {
+                       return View();
+
+        }
+        public ActionResult thong_tin_bai_test()
+        {
+            return View();
+        }
+        public ActionResult end_bai_test()
+        {
+            return View();
+        }
+    }
+}
