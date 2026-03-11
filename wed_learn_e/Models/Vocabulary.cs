e6f3c8032275025e8ecb7eb8bef54b75669d3634
NguyenHieuMinhTam <2424802010435@student.tdmu.edu.vn>
2026-03-12 00:29:06 +0700

    view 1000TuVung

---
+++ wed_learn_e/Models/Vocabulary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wed_learn_e.Models
+{
+    public class Vocabulary
+    {
+        public string Word { get; set; }
+        public string Meaning { get; set; }
+        public string Example { get; set; }
+    }
+}
